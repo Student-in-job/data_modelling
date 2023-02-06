ce60c28ddcb4d317a6bdc957479c0d3829f1c0ef
Vitaliy Pak <vitalik.pak@gmail.com>
2023-02-06 10:02:36 +0900

    initial push

---
+++ MatlabSimple/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+using MathWorks.MATLAB.Engine;
+using MathWorks.MATLAB.Types;
+
+namespace MatlabSimple
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        dynamic engine;
+        double[] rawArray;
+        bool dataRetrieved = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async Task StartMatlabAsync()
+        {
+            engine = await MATLABEngine.StartMATLABAsync();
+        }
+
+        private async void btnStartMatlab_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text = "Starting Matlab...";
+            await StartMatlabAsync();
+            txtOutput.Text = "Matlab have started asynchronously successfully!";
+            btnCalcData.IsEnabled = true;
+        }
+
+        private async Task<double[]> CalculateFunc()
+        {
+            string dir = System.IO.Directory.GetCurrentDirectory();
+            RunOptions opts = new RunOptions(async: true);
+            string command = @"'cd(''" + dir + "'')'";
+            engine.eval(command);
+            double[] retVal = await engine.dft321(opts);
+            return retVal;
+        }
+
+        private void PlotData()
+        {
+            double[] xArray = new double[rawArray.Length];
+            for (int i = 0; i < rawArray.Length; i++)
+            {
+                xArray[i] = i;
+            }
+            Plot pl = new Plot(cvPlot);
+            pl.Clean();
+            pl.PlotData(xArray, rawArray);
+            txtOutput.Text = string.Format("Number of returned values {0}", rawArray.Length.ToString());
+            txtOutput.Text = string.Format("Canvas size - x:{0}, y:{1}", pl.Width.ToString(), pl.Height.ToString());
+        }
+
+        private async void btnCalcData_Click(object sender, RoutedEventArgs e)
+        {
+            rawArray = await CalculateFunc();
+            dataRetrieved = true;
+            PlotData();
+        }
+
+        public void Dispose()
+        {
+            engine.Dispose();
+            MATLABEngine.TerminateEngineClient();
+        }
+
+        private void cvPlot_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (dataRetrieved)
+            {
+                PlotData();
+            }
+        }
+    }
+}
---
+++ MatlabSimple/Plot.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace MatlabSimple
+{
+    public struct Margin
+    {
+        public double left;
+        public double right;
+        public double top;
+        public double bottom;
+
+        public Margin(double left, double right, double top, double bottom)
+        {
+            this.left = left;
+            this.right = right;
+            this.top = top;
+            this.bottom = bottom;
+        }
+
+        public Margin(double leftRight, double topBottom)
+        {
+            this.left = leftRight;
+            this.right = leftRight;
+            this.top = topBottom;
+            this.bottom = topBottom;
+        }
+    }
+
+    public struct Point
+    {
+        public double X;
+        public double Y;
+    }
+
+    public class Plot
+    {
+        #region Fields
+        private Canvas cv;
+        private double biasX;
+        private double biasY;
+        private double leftMargin;
+        private double rightMargin;
+        private double topMargin;
+        private double bottomMargin;
+        private double stepX;
+        private double stepY;
+        #endregion
+
+        #region Properties
+        public double Width
+        {
+            get { return this.cv.ActualWidth - this.leftMargin - this.rightMargin; }
+        }
+        public double Height
+        {
+            get { return this.cv.ActualHeight - this.topMargin - this.bottomMargin; }
+        }
+        #endregion;
+
+        #region Constructors
+        public Plot(Canvas canvas)
+        {
+            this.InitDefaults();
+            this.cv = canvas;
+        }
+
+        public Plot(Canvas canvas, Margin margin)
+        {
+            this.cv = canvas;
+            this.leftMargin = margin.left;
+            this.rightMargin = margin.right;
+            this.topMargin = margin.top;
+            this.bottomMargin = margin.bottom;
+        }
+        #endregion
+
+        #region Methods
+        private void InitDefaults()
+        {
+            leftMargin = 10;
+            rightMargin = 10;
+            topMargin = 10;
+            bottomMargin = 10;
+        }
+        private Point GetPoint(double x, double y)
+        {
+            Point newPoint;
+            newPoint.X = Math.Round(this.biasX + x * stepX) + this.leftMargin;
+            newPoint.Y = this.cv.ActualHeight - (Math.Round(this.biasY + y * stepY) + this.topMargin);
+            return newPoint;
+        }
+        private void drawLine(Point from, Point to)
+        {
+            Line line = new Line();
+            line.Stroke = System.Windows.Media.Brushes.Blue;
+            line.X1 = from.X;
+            line.Y1 = from.Y;
+            line.X2 = to.X;
+            line.Y2 = to.Y;
+            this.cv.Children.Add(line);
+        }
+        public void Clean()
+        {
+            this.cv.Children.Clear();
+        }
+
+        public void PlotData(double[] x, double[] y)
+        {
+            if (x.Length != y.Length)
+            {
+                throw new Exception("Arrays of input values are not same length"); 
+            }
+            double maxX = x.Max();
+            double minX = x.Min();
+            double maxY = y.Max();
+            double minY = y.Min();
+            if (maxX * maxY < 0)
+            {
+                biasX = this.Width / 2;
+                stepX = this.Width / (maxX + Math.Abs(minX));
+            }
+            else
+            {
+                biasX = 0;
+                stepX = this.Width / maxX;
+            }
+            if (maxY * minY < 0)
+            {
+                biasY = this.Height / 2;
+                stepY = this.Height / (maxY + Math.Abs(minY));
+            }
+            else
+            {
+                biasY = 0;
+                stepY = this.Height / maxY;
+            }
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < x.Length; i++)
+            {
+                Point newPoint = this.GetPoint(x[i], y[i]);
+                points.Add(newPoint);
+            }
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                this.drawLine(points[i], points[i + 1]);
+            }
+        }
+        #endregion;
+    }
+}
